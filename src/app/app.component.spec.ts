955d1c052cb36c4d359cbc6d49e5aa36be712c26
TheJose24 <devbyjose@gmail.com>
2026-06-17 21:53:09 -0500

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tech-vision-app' title`, () => {
+  it(`should have the 'salud-ya' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tech-vision-app');
+    expect(app.title).toEqual('salud-ya');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tech-vision-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, salud-ya');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'tech-vision-app';
+  title = 'salud-ya-app';
 }
